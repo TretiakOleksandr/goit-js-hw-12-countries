4dc8ae624c334f0c6abb30c1d4b08e061974173c
TretiakOleksandr <zukaramba@gmail.com>
2021-06-07 10:51:42 +0300

    final

--- src/index.js
+++ src/index.js
@@ -2,20 +2,16 @@ import debounce from 'lodash.debounce';
 
 import './sass/main.scss';
 import countryCardTpl from './templates/country-card.hbs';
+import someCountryTpl from './templates/some-country-list.hbs';
 
-// const refs = {
-//     containerEl: document.querySelector('.js-container'),
-// }
-
-// console.log(refs.containerEl);
 const containerEl = document.querySelector('.js-container');
 const searchEl = document.querySelector('.js-input');
+const listEl = document.querySelector('.js-list');
 
 searchEl.addEventListener('input', debounce(onSearch, 500));
 
 function onSearch(evt) {
     const countryName = evt.target.value;
-    console.log(countryName);
     if (countryName) {
         fetchCountry(countryName)
         .then(renderCountry)
@@ -23,10 +19,24 @@ function onSearch(evt) {
     }
 }
 
-
 function renderCountry(country) {
-    const markup = countryCardTpl(country[0]);
-    containerEl.innerHTML = markup;    
+    
+    if (country.length === 1) {
+        const cardMarkup = countryCardTpl(country[0]);
+        containerEl.innerHTML = cardMarkup;
+        listEl.innerHTML = "";
+    }
+
+    if (country.length < 11 && country.length > 1) {
+        const listMarkup = country.map(someCountryTpl).join('');
+        listEl.innerHTML = listMarkup;
+        containerEl.innerHTML = "";
+    }
+
+    if (country.length > 10) {
+        containerEl.innerHTML = '<h2>Too many matches found.</h2>'
+    }
+    
 }
 
 function fetchCountry(countryName) {
